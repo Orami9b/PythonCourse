5c88e2ff8f605210e84265fc6530c9d0703fba87
oramirez <oscarjoramirez@yahoo.com>
2020-01-01 20:50:57 -0800

    added layer control and modified feature groups

--- project_2/app2.py
+++ project_2/app2.py
@@ -12,7 +12,7 @@ def getColor(elevation):
 
 map = folium.Map(location = (33.44, -112.07), tiles="Stamen Terrain")
 
-featureGroup = folium.FeatureGroup(name = "My Map")
+volcanoLayer = folium.FeatureGroup(name = "Volcanoes")
 volcanoList = pandas.read_csv("Volcanoes.txt")
 html = """
 Volcano name:<br><a href="https://www.google.com/search?q={name}+volcano" target="_blank">{name}</a><br>
@@ -23,15 +23,18 @@ for index, row in volcanoList.iterrows():
    coordinates = (row.LAT, row.LON)
    iFrame = folium.IFrame(html.format(name = row.NAME, elevation = row.ELEV),
       width=200, height=100)
-   featureGroup.add_child(folium.Marker(location = coordinates,
+   volcanoLayer.add_child(folium.Marker(location = coordinates,
       popup = folium.Popup(iFrame, parse_html = True),
       icon = folium.Icon(color = getColor(row.ELEV))))
 
-featureGroup.add_child(folium.GeoJson(data = open("world.json",
+populationLayer = folium.FeatureGroup(name = "Population")
+populationLayer.add_child(folium.GeoJson(data = open("world.json",
    encoding = "utf-8-sig").read(), style_function = 
       lambda x: {"fillColor" : "green" if x["properties"]["POP2005"] > 25000000
       else "orange" if x["properties"]["POP2005"] > 10000000
       else "red"}))
 
-map.add_child(featureGroup)
+map.add_child(volcanoLayer)
+map.add_child(populationLayer)
+map.add_child(folium.LayerControl())
 map.save("Map1.html")
