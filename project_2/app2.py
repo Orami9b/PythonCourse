af75bfc1223bb322a0c1e552360fb6db5f600e61
oramirez <oscarjoramirez@yahoo.com>
2020-01-01 20:45:14 -0800

    added polygon layer with pop data from json file

--- project_2/app2.py
+++ project_2/app2.py
@@ -2,13 +2,13 @@ import folium
 import pandas
 
 def getColor(elevation):
-    if row.ELEV > 3000:
-        elevColor = "red"
-    elif row.ELEV > 2000:
-        elevColor = "orange"
-    else:
-        elevColor = "green"
-    return elevColor
+   if row.ELEV > 3000:
+      elevColor = "red"
+   elif row.ELEV > 2000:
+      elevColor = "orange"
+   else:
+      elevColor = "green"
+   return elevColor
 
 map = folium.Map(location = (33.44, -112.07), tiles="Stamen Terrain")
 
@@ -20,10 +20,18 @@ Height: {elevation} m
 """
 
 for index, row in volcanoList.iterrows():
-    coordinates = (row.LAT, row.LON)
-    iFrame = folium.IFrame(html.format(name = row.NAME, elevation = row.ELEV), width=200, height=100)
-    featureGroup.add_child(folium.Marker(location = coordinates, popup = folium.Popup(iFrame, parse_html = True), icon = folium.Icon(color = getColor(row.ELEV))))
+   coordinates = (row.LAT, row.LON)
+   iFrame = folium.IFrame(html.format(name = row.NAME, elevation = row.ELEV),
+      width=200, height=100)
+   featureGroup.add_child(folium.Marker(location = coordinates,
+      popup = folium.Popup(iFrame, parse_html = True),
+      icon = folium.Icon(color = getColor(row.ELEV))))
+
+featureGroup.add_child(folium.GeoJson(data = open("world.json",
+   encoding = "utf-8-sig").read(), style_function = 
+      lambda x: {"fillColor" : "green" if x["properties"]["POP2005"] > 25000000
+      else "orange" if x["properties"]["POP2005"] > 10000000
+      else "red"}))
 
 map.add_child(featureGroup)
-
 map.save("Map1.html")
