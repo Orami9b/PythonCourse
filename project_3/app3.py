63a315ab491078715133cd1fe95dd11c73ed1cbd
oramirez <oscarjoramirez@yahoo.com>
2020-01-03 15:05:24 -0800

    base logic structure

--- project_3/app3.py
+++ project_3/app3.py
@@ -3,4 +3,21 @@ from datetime import datetime as dateTime
 
 hostPath = "C:\\Windows\\System32\\drivers\\etc"
 redirect = "127.0.0.1"
-websiteList = ["www.facebook.com", "facebook.com", "www.youtube.com", "youtube.come"]
+websiteList = ["www.facebook.com", "facebook.com",
+   "www.youtube.com", "youtube.come"]
+
+workingHoursStart = 8
+workingHoursEnd = 17
+
+while True:
+   year = dateTime.now().year
+   month = dateTime.now().month
+   day = dateTime.now().day
+
+   if (dateTime(year, month, day, workingHoursStart) < dateTime.now()
+         < dateTime(year, month, day, workingHoursEnd)):
+      print("Working hours...")
+   else:
+      print("Fun Hours...")
+      
+   time.sleep(5)   
