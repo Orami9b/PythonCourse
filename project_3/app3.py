88d6c9abb6da478397b5111fe2e36bb95aa90410
oramirez <oscarjoramirez@yahoo.com>
2020-01-03 14:50:00 -0800

    import packages

--- project_3/app3.py
+++ project_3/app3.py
@@ -1,3 +1,6 @@
+import time
+from datetime import datetime as dateTime
+
 hostPath = "C:\\Windows\\System32\\drivers\\etc"
 redirect = "127.0.0.1"
 websiteList = ["www.facebook.com", "facebook.com", "www.youtube.com", "youtube.come"]
