c63342573a11d5a1a2d5a54bc32b8090482dc192
oramirez <oscarjoramirez@yahoo.com>
2020-01-03 16:41:05 -0800

    file writing

--- project_3/app3.py
+++ project_3/app3.py
@@ -1,13 +1,14 @@
 import time
 from datetime import datetime as dateTime
 
+hostTemp = "hosts"
 hostPath = "C:\\Windows\\System32\\drivers\\etc"
 redirect = "127.0.0.1"
 websiteList = ["www.facebook.com", "facebook.com",
-   "www.youtube.com", "youtube.come"]
+   "www.youtube.com", "youtube.com"]
 
-workingHoursStart = 8
-workingHoursEnd = 17
+workingHoursStart = 10
+workingHoursEnd = 12
 
 while True:
    year = dateTime.now().year
@@ -16,8 +17,20 @@ while True:
 
    if (dateTime(year, month, day, workingHoursStart) < dateTime.now()
          < dateTime(year, month, day, workingHoursEnd)):
+      with open(hostPath, "r+") as file:
+         content = file.read()
+         for website in websiteList:
+            if not website in content:
+               file.write(redirect + " " + website + "\n")
       print("Working hours...")
    else:
+      with open(hostPath, "r+") as file:
+         content = file.readlines()
+         file.seek(0)
+         for line in content:
+            if not any(website in line for website in websiteList):
+               file.write(line)
+         file.truncate()
       print("Fun Hours...")
-      
+
    time.sleep(5)   
