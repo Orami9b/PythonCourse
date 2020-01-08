4608bd13a4cfd26993f6f4516a75b118cb159dfd
oramirez <oscarjoramirez@yahoo.com>
2020-01-07 20:56:40 -0800

    complete app

--- gui_practice/practice1.py
+++ gui_practice/practice1.py
@@ -2,6 +2,22 @@ import tkinter as tk
 
 window = tk.Tk()
 
-button1 = tk.Button(window)
+def km_to_miles():
+   input_ = entry1_value.get()
+   if (any(i.isdigit() for i in input_)):
+      output = str(float(input_) * 1.6)
+   else:
+      output = input_
+   text1.insert(tk.END, output + "\n")
+
+button1 = tk.Button(window, text = "Execute", command = km_to_miles)
+button1.grid(row = 0, column = 0)
+
+entry1_value = tk.StringVar()
+entry1 = tk.Entry(window, textvariable = entry1_value)
+entry1.grid(row = 0, column = 1)
+
+text1 = tk.Text(window, height = 5, width = 20)
+text1.grid(row = 0, column = 2)
 
 window.mainloop()
